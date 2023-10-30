aacd339108bde5b2caafb1646a0a09c3bfa664c2
sa96125 <sa96125@gmail.co.kr>
2023-10-30 20:18:03 +0900

    [feat] 글로벌 스타일 추가

---
+++ src/common/style/index.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const TAB_BAR_HEIGHT = "4.5rem";
+
+export const Contents = styled(Box)(() => ({
+  margin: "1rem",
+  height: "200vh",
+  mb: TAB_BAR_HEIGHT,
+}));
